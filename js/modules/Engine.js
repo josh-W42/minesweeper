c3a78ee5c1c4f344b6199a5371e5cce5dced4c0e
jwilson42 <wilson.jd022@gmail.com>
2021-01-24 14:12:39 -0800

    wrote documentation for engine class

--- js/modules/Engine.js
+++ js/modules/Engine.js
@@ -4,18 +4,31 @@
 */
 import { setupCanvas } from "./canvas.js";
 
+/**
+ * Runs processes to begin the game.
+ * @param {Number} id - Difficulty Identifier.
+ */
 const startGame = id => {
     setupCanvas(id);
 }
 
+/**
+ * Runs all processes when the game has ended.
+ */
 const endGame = () => {
     console.log('Game Over');
 }
 
+/**
+ * Sets up end game sceen for a player win state.
+ */
 const playerWin = () => {
     console.log('Win');
 }
 
+/**
+ * Sets up end game sceen for a player lose state.
+ */
 const playerLost = () => {
     console.log('Lost');
 }
